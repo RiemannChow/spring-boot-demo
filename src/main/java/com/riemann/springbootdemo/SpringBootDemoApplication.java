6477545fe4f9e2622054357728031ab230607ac7
riemann <754798352@qq.com>
2019-06-15 23:23:37 +0800

    SpringBootDemoApplication

--- src/main/java/com/riemann/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/riemann/springbootdemo/SpringBootDemoApplication.java
@@ -1,11 +1,15 @@
 package com.riemann.springbootdemo;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.riemann.springbootdemo.dao")
+@ComponentScan(value = "com.riemann")
+@EnableEncryptableProperties //启动数据库加密功能
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
