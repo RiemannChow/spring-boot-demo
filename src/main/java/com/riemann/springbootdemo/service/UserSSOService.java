f4ba4451790c0dfbbf134ad31cd61c15008a6c22
riemann <754798352@qq.com>
2019-07-17 21:21:52 +0800

    UserSSOService

---
+++ src/main/java/com/riemann/springbootdemo/service/UserSSOService.java
@@ -0,0 +1,26 @@
+package com.riemann.springbootdemo.service;
+
+import com.riemann.springbootdemo.model.ResultSSO;
+import com.riemann.springbootdemo.model.UserSSO;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author riemann
+ * @date 2019/06/29 15:46
+ */
+@Component
+public interface UserSSOService {
+
+    ResultSSO registerUser(UserSSO userSSO);
+
+    ResultSSO userLogin(String account, String password,
+                        HttpServletRequest request, HttpServletResponse response);
+
+    void logout(String token);
+
+    ResultSSO queryUserByToken(String token);
+
+}
