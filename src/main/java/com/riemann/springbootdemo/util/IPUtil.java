06216e04c2b569b51ef414eb3a9f5ae1cbeb3493
riemann <754798352@qq.com>
2019-08-11 15:43:11 +0800

    判断IP是否在指定范围

--- src/main/java/com/riemann/springbootdemo/util/IPUtil.java
+++ src/main/java/com/riemann/springbootdemo/util/IPUtil.java
@@ -20,7 +20,7 @@ import java.net.URLConnection;
 public class IPUtil {
 
     /**
-     * 获取真实的ip
+     * 获取真实的IP
      * @param request
      * @return
      */
@@ -40,6 +40,48 @@ public class IPUtil {
             return ip;
         }
         return request.getRemoteAddr();
+    }
+
+    /**
+     * 判断IP是否在指定范围
+     * @param ipStart
+     * @param ipEnd
+     * @param ip
+     * @return
+     */
+    public static boolean ipIsValid(String ipStart,String ipEnd, String ip) {
+        if (StringUtils.isEmpty(ipStart)) {
+            throw new NullPointerException("起始IP不能为空！");
+        }
+        if (StringUtils.isEmpty(ipEnd)) {
+            throw new NullPointerException("结束IP不能为空！");
+        }
+        if (StringUtils.isEmpty(ip)) {
+            throw new NullPointerException("IP不能为空！");
+        }
+        ipStart = ipStart.trim();
+        ipEnd = ipEnd.trim();
+        ip = ip.trim();
+        final String REGX_IP = "((25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]\\d|\\d)\\.){3}(25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]\\d|\\d)";
+        final String REGX_IPB = REGX_IP + "\\-" + REGX_IP;
+        if (!ipStart.matches(REGX_IP) || !ip.matches(REGX_IP) || !ipEnd.matches(REGX_IP)) {
+            return false;
+        }
+        String[] sips = ipStart.split("\\.");
+        String[] sipe = ipEnd.split("\\.");
+        String[] sipt = ip.split("\\.");
+        long ips = 0L, ipe = 0L, ipt = 0L;
+        for (int i = 0; i < 4; ++i) {
+            ips = ips << 8 | Integer.parseInt(sips[i]);
+            ipe = ipe << 8 | Integer.parseInt(sipe[i]);
+            ipt = ipt << 8 | Integer.parseInt(sipt[i]);
+        }
+        if (ips > ipe) {
+            long t = ips;
+            ips = ipe;
+            ipe = t;
+        }
+        return ips <= ipt && ipt <= ipe;
     }
 
 }
