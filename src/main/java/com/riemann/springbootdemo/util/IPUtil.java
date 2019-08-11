5dac2a79f9f7c0ded290b0e9600b95ab0c9b0a32
riemann <754798352@qq.com>
2019-08-11 15:41:55 +0800

    获取真实ip的工具类

---
+++ src/main/java/com/riemann/springbootdemo/util/IPUtil.java
@@ -0,0 +1,46 @@
+package com.riemann.springbootdemo.util;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * @author riemann
+ * @date 2019/08/10 17:20
+ */
+public class IPUtil {
+
+    /**
+     * 获取真实的ip
+     * @param request
+     * @return
+     */
+    public static String getIp(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (StringUtils.isNotEmpty(ip) && !"unKnown".equalsIgnoreCase(ip)) {
+            //多次反向代理后会有多个ip值，第一个ip才是真实ip
+            int index = ip.indexOf(",");
+            if (index != -1) {
+                return ip.substring(0,index);
+            } else {
+                return ip;
+            }
+        }
+        ip = request.getHeader("X-Real-IP");
+        if (StringUtils.isNotEmpty(ip) && !"unKnown".equalsIgnoreCase(ip)) {
+            return ip;
+        }
+        return request.getRemoteAddr();
+    }
+
+}
+
