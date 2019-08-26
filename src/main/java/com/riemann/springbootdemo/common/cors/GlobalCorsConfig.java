1527d6ca693b0a55bcb3333a8d8d01452cd0ea17
riemann <754798352@qq.com>
2019-08-26 23:09:23 +0800

    GlobalCorsConfig

--- src/main/java/com/riemann/springbootdemo/common/cors/GlobalCorsConfig.java
+++ src/main/java/com/riemann/springbootdemo/common/cors/GlobalCorsConfig.java
@@ -1,11 +1,12 @@
 package com.riemann.springbootdemo.common.cors;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author riemann
@@ -14,6 +15,7 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class GlobalCorsConfig {
 
+    // 第一种方式:返回新的CorsFilter(全局跨域)
     @Bean
     public CorsFilter corsFilter() {
         //1.添加CORS配置信息
@@ -35,6 +37,28 @@ public class GlobalCorsConfig {
 
         //3.返回新的CorsFilter.
         return new CorsFilter(configSource);
+    }
+
+    // 第二种方式:重写WebMvcConfigurer（全局跨域）
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            //重写父类提供的跨域请求处理的接口
+            public void addCorsMappings(CorsRegistry registry) {
+                //添加映射路径
+                registry.addMapping("/**")
+                        //放行哪些原始域
+                        .allowedOrigins("/*")
+                        //是否发送Cookie信息
+                        .allowCredentials(true)
+                        //放行哪些原始域(请求方式)
+                        .allowedMethods("GET","POST", "PUT", "DELETE")
+                        //放行哪些原始域(头部信息)
+                        .allowedHeaders("*")
+                        //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
+                        .exposedHeaders("Header1", "Header2");
+            }
+        };
     }
 
 }
