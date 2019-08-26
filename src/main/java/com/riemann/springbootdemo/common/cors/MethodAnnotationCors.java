39a7e86f078c17d14ea52649c68d626f8b0450b1
riemann <754798352@qq.com>
2019-08-26 23:25:38 +0800

    MethodAnnotationCors

---
+++ src/main/java/com/riemann/springbootdemo/common/cors/MethodAnnotationCors.java
@@ -0,0 +1,23 @@
+package com.riemann.springbootdemo.common.cors;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author riemann
+ * @date 2019/08/26 23:16
+ */
+// 2、在方法上注解
+@Controller
+public class MethodAnnotationCors {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    @CrossOrigin("http://localhost:8080")
+    public String index( ){
+        return "Hello World";
+    }
+
+}
