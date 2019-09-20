cf0666e7c491b678243f30878cae6a0314b042c4
riemann <754798352@qq.com>
2019-09-20 21:26:33 +0800

    MyAdapterConfig

---
+++ src/main/java/com/riemann/springbootdemo/interceptor/MyAdapterConfig.java
@@ -0,0 +1,22 @@
+package com.riemann.springbootdemo.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author riemann
+ * @date 2019/07/20 12:03
+ */
+public class MyAdapterConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private UserLoginHandlerInterceptor userLoginHandlerInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 判断用户是否登录的拦截器
+        registry.addInterceptor(userLoginHandlerInterceptor).addPathPatterns("/mygithub/**");
+    }
+
+}
