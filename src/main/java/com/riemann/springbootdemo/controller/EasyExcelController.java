08f0f3ae9409b6c73bb125732084ddd2a5eba60c
riemann <754798352@qq.com>
2019-12-21 21:17:08 +0800

    add downloadEasyExcel method

--- src/main/java/com/riemann/springbootdemo/controller/EasyExcelController.java
+++ src/main/java/com/riemann/springbootdemo/controller/EasyExcelController.java
@@ -3,6 +3,9 @@ package com.riemann.springbootdemo.controller;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelReader;
 import com.alibaba.excel.read.metadata.ReadSheet;
+import com.alibaba.excel.write.metadata.style.WriteCellStyle;
+import com.alibaba.excel.write.metadata.style.WriteFont;
+import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
 import com.riemann.springbootdemo.listener.UploadEasyExcelListener;
 import com.riemann.springbootdemo.model.ReturnT;
 import com.riemann.springbootdemo.model.UploadEasyExcelData;
@@ -15,14 +18,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author riemann
@@ -40,7 +49,7 @@ public class EasyExcelController {
     @ApiOperation(value = "上传excel", notes = "通过easy excel上传excel到db")
     @ApiImplicitParam(name = "serviceFile", value = "excel文件", paramType = "save", required = true)
     @PostMapping(value = "/uploadEasyExcel",produces = "application/json;charset=UTF-8")
-    public ResponseEntity<ReturnT<String>> UploadEasyExcel(@RequestParam(value = "serviceFile") MultipartFile serviceFile) {
+    public ResponseEntity<ReturnT<String>> uploadEasyExcel(@RequestParam(value = "serviceFile") MultipartFile serviceFile) {
         if (serviceFile == null) {
             return new ResponseEntity<>(new ReturnT<>(ReturnT.BAD_REQUEST, "Params can not be null"), HttpStatus.BAD_REQUEST);
         }
@@ -52,8 +61,8 @@ public class EasyExcelController {
                     new UploadEasyExcelListener(ueeService)).build();
             ReadSheet readSheet = EasyExcel.readSheet(0).build();
             excelReader.read(readSheet);
-        } catch (IOException ex) {
-            LOGGER.error("import excel to db fail", ex);
+        } catch (IOException e) {
+            LOGGER.error("import excel to db fail", e);
         } finally {
             close(in);
             // 这里一定别忘记关闭，读的时候会创建临时文件，到时磁盘会崩
@@ -72,6 +81,42 @@ public class EasyExcelController {
                 LOGGER.error("Close io stream error", e);
             }
         }
+    }
+
+    @ApiOperation(value = "下载excel", notes = "通过easy excel从db下载数据到excel")
+    @GetMapping(value = "/downloadEasyExcel",produces = "application/json;charset=UTF-8")
+    public void downloadEasyExcel(HttpServletResponse response) throws IOException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String datetime = sdf.format(new Date());
+        String fileName = URLEncoder.encode("下载excel服务", "UTF-8") + datetime;
+
+        response.setContentType("application/vnd.ms-excel");
+        response.setCharacterEncoding("utf-8");
+        response.setHeader("Content-disposition", "attachment;filename=" + fileName + ".xlsx");
+
+        // excel头策略
+        WriteCellStyle headWriteCellStyle = new WriteCellStyle();
+        WriteFont headWriteFont = new WriteFont();
+        headWriteFont.setFontHeightInPoints((short) 11);
+        headWriteFont.setBold(false);
+        headWriteCellStyle.setWriteFont(headWriteFont);
+
+        // excel内容策略
+        WriteCellStyle contentWriteCellStyle = new WriteCellStyle();
+        WriteFont contentWriteFont = new WriteFont();
+        contentWriteFont.setFontHeightInPoints((short)11);
+        contentWriteCellStyle.setWriteFont(contentWriteFont);
+
+        // 设置handler
+        HorizontalCellStyleStrategy styleStrategy =
+                new HorizontalCellStyleStrategy(headWriteCellStyle, contentWriteCellStyle);
+
+        List<UploadEasyExcelData> uploadEasyExcelData = ueeService.selectAll();
+
+        EasyExcel.write(response.getOutputStream(), UploadEasyExcelData.class)
+                .sheet("下载excel服务")
+                .registerWriteHandler(styleStrategy)
+                .doWrite(uploadEasyExcelData);
     }
 }
 
