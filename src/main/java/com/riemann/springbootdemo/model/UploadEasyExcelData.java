71368b031b74a32a562e8a2879c649889c1db314
riemann <754798352@qq.com>
2019-12-21 19:46:41 +0800

    UploadEasyExcelData

---
+++ src/main/java/com/riemann/springbootdemo/model/UploadEasyExcelData.java
@@ -0,0 +1,34 @@
+package com.riemann.springbootdemo.model;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author riemann
+ * @date 2019/12/19 23:08
+ */
+@NoArgsConstructor
+@Data
+@Getter
+@Setter
+public class UploadEasyExcelData {
+
+    @ExcelProperty(value = "name", index = 0)
+    private String name;
+
+    @ExcelProperty(value = "sex", index = 1)
+    private String sex;
+
+    @ExcelProperty(value = "age", index = 2)
+    private Integer age;
+
+    @ExcelProperty(value = "address", index = 3)
+    private String address;
+
+    @ExcelProperty(value = "phone", index = 4)
+    private String phone;
+
+}
