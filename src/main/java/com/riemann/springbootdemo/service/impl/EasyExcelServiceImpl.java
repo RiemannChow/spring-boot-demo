3756292f989a611602d7b51b1ce5b0527022f16c
riemann <754798352@qq.com>
2019-12-23 19:55:45 +0800

    EasyExcelServiceImpl

--- src/main/java/com/riemann/springbootdemo/service/impl/EasyExcelServiceImpl.java
+++ src/main/java/com/riemann/springbootdemo/service/impl/EasyExcelServiceImpl.java
@@ -1,8 +1,8 @@
 package com.riemann.springbootdemo.service.impl;
 
 import com.riemann.springbootdemo.dao.EasyExcelDao;
-import com.riemann.springbootdemo.model.UploadEasyExcelData;
-import com.riemann.springbootdemo.service.UploadEasyExcelService;
+import com.riemann.springbootdemo.model.EasyExcelData;
+import com.riemann.springbootdemo.service.EasyExcelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,18 +13,18 @@ import java.util.List;
  * @date 2019/12/19 23:40
  */
 @Service
-public class UploadEasyExcelServiceImpl implements UploadEasyExcelService {
+public class EasyExcelServiceImpl implements EasyExcelService {
 
     @Autowired
     private EasyExcelDao easyExcelDao;
 
     @Override
-    public void saveEasyExcelMappingData(List<UploadEasyExcelData> ueeDatas) {
+    public void saveEasyExcelMappingData(List<EasyExcelData> ueeDatas) {
         easyExcelDao.saveEasyExcelMappingData(ueeDatas);
     }
 
     @Override
-    public List<UploadEasyExcelData> selectAll() {
+    public List<EasyExcelData> selectAll() {
         return easyExcelDao.selectAll();
     }
 }
