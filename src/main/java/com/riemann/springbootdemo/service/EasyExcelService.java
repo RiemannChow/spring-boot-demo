49627332b3992a1c3f4bf1c768716ee198ea6746
riemann <754798352@qq.com>
2019-12-23 19:54:53 +0800

    EasyExcelService

--- src/main/java/com/riemann/springbootdemo/service/EasyExcelService.java
+++ src/main/java/com/riemann/springbootdemo/service/EasyExcelService.java
@@ -1,6 +1,6 @@
 package com.riemann.springbootdemo.service;
 
-import com.riemann.springbootdemo.model.UploadEasyExcelData;
+import com.riemann.springbootdemo.model.EasyExcelData;
 
 import java.util.List;
 
@@ -8,18 +8,18 @@ import java.util.List;
  * @author riemann
  * @date 2019/12/19 23:31
  */
-public interface UploadEasyExcelService {
+public interface EasyExcelService {
 
     /**
      * 通过easy excel上传excel数据至mysql
      * @param ueeDatas
      */
-    void saveEasyExcelMappingData(List<UploadEasyExcelData> ueeDatas);
+    void saveEasyExcelMappingData(List<EasyExcelData> ueeDatas);
 
     /**
      *查询所有
      * @return
      */
-    List<UploadEasyExcelData> selectAll();
+    List<EasyExcelData> selectAll();
 
 }
