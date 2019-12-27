b5a175e4619a5f08642751c7e3b8438b4ed4d9f4
riemann <754798352@qq.com>
2019-12-27 21:10:12 +0800

    SpringBootDemoApplication

--- src/main/java/com/riemann/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/riemann/springbootdemo/SpringBootDemoApplication.java
@@ -4,12 +4,16 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("com.riemann.springbootdemo.dao")
 @ComponentScan(value = "com.riemann")
 @EnableEncryptableProperties //启动数据库加密功能
+@ServletComponentScan
+@EnableSwagger2
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
