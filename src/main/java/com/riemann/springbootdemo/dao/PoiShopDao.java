9b70f39f31626bc73313887b9264d825b64bd03c
riemann <754798352@qq.com>
2020-02-14 22:18:01 +0800

    PoiShopDao

---
+++ src/main/java/com/riemann/springbootdemo/dao/PoiShopDao.java
@@ -0,0 +1,18 @@
+package com.riemann.springbootdemo.dao;
+
+import com.riemann.springbootdemo.model.PoiShopEntity;
+
+import java.util.List;
+
+/**
+ * @author riemann
+ * @date 2020/02/14 21:21
+ */
+public interface PoiShopDao {
+
+    Integer uploadShopData(List<PoiShopEntity> list);
+
+    Integer updateBatchShopData(List<PoiShopEntity> list);
+
+    List<PoiShopEntity> selectSelectiveList(List<PoiShopEntity> shopEntityList);
+}
