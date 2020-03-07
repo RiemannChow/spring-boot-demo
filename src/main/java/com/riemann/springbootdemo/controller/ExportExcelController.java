2b02d09fc62c015dfedac5da2bbb0b80dabe5bce
riemann <754798352@qq.com>
2020-03-07 11:03:54 +0800

    ExportExcelController

--- src/main/java/com/riemann/springbootdemo/controller/ExportExcelController.java
+++ src/main/java/com/riemann/springbootdemo/controller/ExportExcelController.java
@@ -1,34 +1,35 @@
 package com.riemann.springbootdemo.controller;
 
-/*import com.riemann.springbootdemo.model.ApiResponse;
+import com.riemann.springbootdemo.model.ApiResult;
 import com.riemann.springbootdemo.model.ExportExcelData;
 import com.riemann.springbootdemo.service.ExportExcelService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 
-*//**
+
+/**
  * Excel导出 Controller
  *
  * @author riemann
  * @date 2019/04/28 21:37
- *//*
+ */
 @RestController
 @RequestMapping(value = "/export")
 public class ExportExcelController {
 
-    @Autowired
+    @Resource
     private ExportExcelService exportExcelService;
 
     @RequestMapping(value = "/exportExcel", method= RequestMethod.POST)
-    public ApiResponse exportExcel(@RequestBody ExportExcelData eeData) {
-        ApiResponse apiResponse = new ApiResponse();
+    public ApiResult exportExcel(@RequestBody ExportExcelData eeData) {
+        ApiResult apiResult = new ApiResult();
         if (eeData == null) {
             return null;
         } else {
-            apiResponse = exportExcelService.exportExcel(eeData);
+            apiResult = exportExcelService.exportExcel(eeData);
         }
-        return apiResponse;
+        return apiResult;
     }
 
-}*/
+}
