695d0f0543dabf68fa1d647c6e7074ce98435c24
riemann <754798352@qq.com>
2020-03-07 11:04:44 +0800

    ObjectConvertXMLController

--- src/main/java/com/riemann/springbootdemo/controller/ObjectConvertXMLController.java
+++ src/main/java/com/riemann/springbootdemo/controller/ObjectConvertXMLController.java
@@ -1,10 +1,7 @@
 package com.riemann.springbootdemo.controller;
 
-import com.riemann.springbootdemo.model.ApiResponse;
-import com.riemann.springbootdemo.model.ExportExcelData;
+import com.riemann.springbootdemo.model.ApiResult;
 import com.riemann.springbootdemo.model.objectConvertXML.Policy;
-import com.riemann.springbootdemo.service.impl.EmailTask;
-import com.riemann.springbootdemo.util.ConstantsUtil;
 import com.riemann.springbootdemo.util.XMLUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,7 +21,7 @@ public class ObjectConvertXMLController {
     private static final Logger logger = LoggerFactory.getLogger(ObjectConvertXMLController.class);
 
     @RequestMapping(value = "/objectToXML", method= RequestMethod.POST)
-    public ApiResponse objectToXML(@RequestBody Policy policy) {
+    public ApiResult objectToXML(@RequestBody Policy policy) {
         String path = "D:\\Policy.xml";
         logger.info("---将对象转换成File类型的xml Start---");
         String str = XMLUtil.convertToXml(policy, path);
@@ -35,6 +32,6 @@ public class ObjectConvertXMLController {
         Policy policyObj = (Policy) XMLUtil.convertXmlFileToObject(Policy.class, path);
         logger.info(policyObj.toString());
         logger.info("---将File类型的xml转换成对象 End---");
-        return new ApiResponse(ConstantsUtil.SUCCESS_CODE, null, ConstantsUtil.SUCCESS);
+        return new ApiResult();
     }
 }
