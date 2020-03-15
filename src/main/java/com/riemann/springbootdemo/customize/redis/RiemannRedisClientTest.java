56f21cfaf579107d6c34f2475e8538892ec6880b
riemann <754798352@qq.com>
2020-03-15 18:18:07 +0800

    RiemannRedisClientTest

--- src/main/java/com/riemann/springbootdemo/customize/redis/RiemannRedisClientTest.java
+++ src/main/java/com/riemann/springbootdemo/customize/redis/RiemannRedisClientTest.java
@@ -1,11 +1,12 @@
 package com.riemann.springbootdemo.customize.redis;
 
-import redis.clients.jedis.Jedis;
-
 public class RiemannRedisClientTest {
     public static void main(String[] args) {
-        //Jedis jedis = new Jedis("192.168.0.105", 6380);
-        Jedis jedis = new Jedis("192.168.0.105", 6379);
-        jedis.set("riemann", "0328");
+//        Jedis jedis = new Jedis("192.168.0.105", 6379);
+//        jedis.set("riemann", "0328");
+
+        RiemannRedisClient client = new RiemannRedisClient("192.168.0.105", 6379);
+        client.set("riemann", "0328");
+        System.out.println(client.get("riemann"));
     }
 }
