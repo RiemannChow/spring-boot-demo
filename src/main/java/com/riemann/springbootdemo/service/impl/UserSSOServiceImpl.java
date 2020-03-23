848a8c8cd7ea6e8fa2e42c90348876ac4d265942
riemann <754798352@qq.com>
2020-03-23 21:37:12 +0800

    UserSSOServiceImpl

--- src/main/java/com/riemann/springbootdemo/service/impl/UserSSOServiceImpl.java
+++ src/main/java/com/riemann/springbootdemo/service/impl/UserSSOServiceImpl.java
@@ -6,11 +6,13 @@ import com.riemann.springbootdemo.repository.JedisClient;
 import com.riemann.springbootdemo.repository.UserSSORepository;
 import com.riemann.springbootdemo.service.UserSSOService;
 import com.riemann.springbootdemo.util.CookieUtil;
+import com.riemann.springbootdemo.util.HttpClientUtil;
 import com.riemann.springbootdemo.util.JsonUtil;
 import com.riemann.springbootdemo.util.SSOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -25,7 +27,7 @@ import java.util.UUID;
  */
 @Service
 @Transactional
-@PropertySource(value = "classpath:redis.properties")
+@PropertySources({@PropertySource(value = "classpath:redis.properties"),@PropertySource(value = "classpath:resource.properties")})
 public class UserSSOServiceImpl implements UserSSOService {
 
     @Autowired
@@ -39,6 +41,18 @@ public class UserSSOServiceImpl implements UserSSOService {
 
     @Value("${SSO_SESSION_EXPIRE}")
     private Integer SSO_SESSION_EXPIRE;
+
+    @Value("${SSO_BASE_URL}")
+    public String SSO_BASE_URL;
+
+    @Value("${SSO_DOMAIN_BASE_USRL}")
+    public String SSO_DOMAIN_BASE_USRL;
+
+    @Value("${SSO_USER_TOKEN}")
+    private String SSO_USER_TOKEN;
+
+    @Value("${SSO_PAGE_LOGIN}")
+    public String SSO_PAGE_LOGIN;
 
     @Override
     public ResultSSO registerUser(UserSSO userSSO) {
@@ -66,7 +80,7 @@ public class UserSSOServiceImpl implements UserSSOService {
         userSSO.setPassword(null);
         userSSO.setSalt(null);
         // 把用户信息写入 redis
-        jedisClient.set(REDIS_USER_SESSION_KEY + ":" + token, JsonUtil.objectToJson(userSSO));
+        jedisClient.set(REDIS_USER_SESSION_KEY + ":" + token, JsonUtil.bean2Json(userSSO));
         // userSSO 已经是持久化对象了，被保存在了session缓存当中，若userSSO又重新修改了属性值，那么在提交事务时，此时 hibernate对象就会拿当前这个user对象和保存在session缓存中的user对象进行比较，如果两个对象相同，则不会发送update语句，否则，如果两个对象不同，则会发出update语句。
         userSSO.setPassword(userPassword);
         userSSO.setSalt(userSalt);
@@ -95,8 +109,27 @@ public class UserSSOServiceImpl implements UserSSOService {
         // 更新过期时间
         jedisClient.expire(REDIS_USER_SESSION_KEY + ":" + token, SSO_SESSION_EXPIRE);
         // 返回用户信息
-        return ResultSSO.ok(JsonUtil.jsonToPojo(json, UserSSO.class));
+        return ResultSSO.ok(JsonUtil.json2Bean(json, UserSSO.class));
 
+    }
+
+    @Override
+    public UserSSO getUserByToken(String token) {
+        try {
+            // 调用sso系统的服务，根据token取用户信息
+            String json = HttpClientUtil.doGet(SSO_BASE_URL + SSO_USER_TOKEN + token);
+            System.out.println("json : " + json);
+            // 把json转换成ItdragonResult
+            ResultSSO result = ResultSSO.formatToModel(json, UserSSO.class);
+            if (null != result && result.getStatus() == 200) {
+                UserSSO userSSO = (UserSSO) result.getData();
+                return userSSO;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
