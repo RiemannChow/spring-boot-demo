697513d745f1c10d11f3518be4efde93ffe3515c
riemann <754798352@qq.com>
2020-04-04 22:13:51 +0800

    RedisConfig

--- src/main/java/com/riemann/springbootdemo/config/RedisConfig.java
+++ src/main/java/com/riemann/springbootdemo/config/RedisConfig.java
@@ -3,31 +3,104 @@ package com.riemann.springbootdemo.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * @author riemann
  * @date 2020/03/30 20:54
  */
 @Configuration
+@PropertySource("classpath:redis-sentinel.properties")
 public class RedisConfig {
 
+    @Value("${redis.maxIdle}")
+    private Integer maxIdle;
+
+    @Value("${redis.maxTotal}")
+    private Integer maxTotal;
+
+    @Value("${redis.maxWaitMillis}")
+    private Integer maxWaitMillis;
+
+    @Value("${redis.minEvictableIdleTimeMillis}")
+    private Integer minEvictableIdleTimeMillis;
+
+    @Value("${redis.numTestsPerEvictionRun}")
+    private Integer numTestsPerEvictionRun;
+
+    @Value("${redis.timeBetweenEvictionRunsMillis}")
+    private long timeBetweenEvictionRunsMillis;
+
+    @Value("${redis.testOnBorrow}")
+    private boolean testOnBorrow;
+
+    @Value("${redis.testWhileIdle}")
+    private boolean testWhileIdle;
+
+    @Value("${redis.hostName}")
+    private String hostName;
+
+    @Value("${redis.port}")
+    private Integer port;
+
+    @Value("${redis.timeout}")
+    private Integer timeout;
+
+    @Value("${spring.redis.sentinel.master}")
+    private String master;
+
+    @Value("${spring.redis.sentinel.nodes}")
+    private String redisNodes;
+
     /**
+     * JedisPoolConfig 连接池
+     * @return
+     */
+    @Bean
+    public JedisPoolConfig jedisPoolConfig() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        // 最大空闲数
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        // 连接池的最大数据库连接数
+        jedisPoolConfig.setMaxTotal(maxTotal);
+        // 最大建立连接等待时间
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+        // 逐出连接的最小空闲时间 默认1800000毫秒(30分钟)
+        jedisPoolConfig.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        // 每次逐出检查时 逐出的最大数目 如果为负数就是 : 1/abs(n), 默认3
+        jedisPoolConfig.setNumTestsPerEvictionRun(numTestsPerEvictionRun);
+        // 逐出扫描的时间间隔(毫秒) 如果为负数,则不运行逐出线程, 默认-1
+        jedisPoolConfig.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        // 是否在从池中取出连接前进行检验,如果检验失败,则从池中去除连接并尝试取出另一个
+        jedisPoolConfig.setTestOnBorrow(testOnBorrow);
+        // 在空闲时检查有效性, 默认false
+        jedisPoolConfig.setTestWhileIdle(testWhileIdle);
+        return jedisPoolConfig;
+    }
+
+    /**
+     * redisTemplate 序列化使用的jdkSerializeable, 存储二进制字节码, 所以自定义序列化类
+     *
      * @SuppressWarnings 该批注的作用是给编译器一条指令，告诉它对被批注的代码元素内部的某些警告保持静默
      *
-     *    关键字         用途
-     *    deprecation   使用了不赞成使用的类或方法时的警告
-     *    unchecked     执行了未检查的转换时的警告，例如当使用集合时没有用泛型 (Generics) 来指定集合保存的类型。
-     *    fallthrough   当 Switch 程序块直接通往下一种情况而没有 Break 时的警告。
-     *    path          在类路径、源文件路径等中有不存在的路径时的警告。
-     *    serial        当在可序列化的类上缺少 serialVersionUID 定义时的警告。
-     *    finally       任何 finally 子句不能正常完成时的警告。
-     *    all           关于以上所有情况的警告。
+     * 关键字         用途
+     * deprecation   使用了不赞成使用的类或方法时的警告
+     * unchecked     执行了未检查的转换时的警告，例如当使用集合时没有用泛型 (Generics) 来指定集合保存的类型。
+     * fallthrough   当 Switch 程序块直接通往下一种情况而没有 Break 时的警告。
+     * path          在类路径、源文件路径等中有不存在的路径时的警告。
+     * serial        当在可序列化的类上缺少 serialVersionUID 定义时的警告。
+     * finally       任何 finally 子句不能正常完成时的警告。
+     * all           关于以上所有情况的警告。
      */
 
     // 注入 Spring 容器中，忽略所有警告
@@ -52,6 +125,24 @@ public class RedisConfig {
         template.setHashValueSerializer(jackson2JsonRedisSerializer);
         template.afterPropertiesSet();
         return template;
+    }
+
+    /**
+     * redis哨兵配置
+     * @return
+     */
+    @Bean
+    public RedisSentinelConfiguration redisSentinelConfiguration() {
+        RedisSentinelConfiguration configuration = new RedisSentinelConfiguration();
+        String[] host = redisNodes.split(",");
+        for(String redisHost : host) {
+            String[] item = redisHost.split(":");
+            String ip = item[0];
+            String port = item[1];
+            configuration.addSentinel(new RedisNode(ip, Integer.parseInt(port)));
+        }
+        configuration.setMaster(master);
+        return configuration;
     }
 
 }
