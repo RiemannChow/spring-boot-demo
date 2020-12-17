5531cc68b84871c36100dc4a78233f882a72b9ff
riemann <754798352@qq.com>
2020-12-17 23:36:39 +0800

    CommonVOCode

---
+++ src/main/java/com/riemann/springbootdemo/model/vo/CommonVOCode.java
@@ -0,0 +1,11 @@
+package com.riemann.springbootdemo.model.vo;
+
+public class CommonVOCode {
+
+    public final static Integer SUCCESS = 200;
+
+    public final static Integer BAD_PARAMS = 400;
+
+    public final static Integer SERVER_ERROR = 500;
+
+}
